5c3ba1f4fd6749b79c8ec8667c9c63ee8f44789e
Eduardo Amorim <atenas503@gmail.com>
2024-02-25 10:32:13 -0300

    refactor: Usa partição nas queries quando possível

--- analise_sql.sql
+++ analise_sql.sql
@@ -1,15 +1,15 @@
 -- 1) Quantos chamados foram abertos no dia 01/04/2023?
 -- Resposta: 73 chamados.
 -- Query:
-SELECT COUNT(*) AS quantidade_chamados FROM datario.administracao_servicos_publicos.chamado_1746 
-WHERE DATE(data_inicio) = '2023-04-01';
+SELECT COUNT(*) AS quantidade_chamados FROM datario.administracao_servicos_publicos.chamado_1746
+WHERE DATE(data_inicio) = '2023-04-01' AND data_particao = '2023-04-01';
 
 
 -- 2) Qual o tipo de chamado que teve mais teve chamados abertos no dia 01/04/2023?
 -- Resposta: Poluição Sonora.
 -- Query:
 SELECT tipo FROM datario.administracao_servicos_publicos.chamado_1746 
-WHERE DATE(data_inicio) = '2023-04-01'
+WHERE DATE(data_inicio) = '2023-04-01' AND data_particao = '2023-04-01'
 GROUP BY tipo
 ORDER BY COUNT(*) DESC
 LIMIT 1;
@@ -18,9 +18,9 @@ LIMIT 1;
 -- 3) Quais os nomes dos 3 bairros que mais tiveram chamados abertos nesse dia?
 -- Resposta: Engenho de Dentro, Leblon, Campo Grande.
 -- Query:
-SELECT bairros.nome as nome_bairro FROM datario.administracao_servicos_publicos.chamado_1746 AS chamados
-JOIN datario.dados_mestres.bairro as bairros ON chamados.id_bairro = bairros.id_bairro
-WHERE DATE(data_inicio) = '2023-04-01'
+SELECT bairros.nome AS nome_bairro FROM datario.administracao_servicos_publicos.chamado_1746 AS chamados
+JOIN datario.dados_mestres.bairro AS bairros ON chamados.id_bairro = bairros.id_bairro
+WHERE DATE(data_inicio) = '2023-04-01' AND data_particao = '2023-04-01'
 GROUP BY bairros.nome
 ORDER BY COUNT(*) DESC
 LIMIT 3;
@@ -29,9 +29,9 @@ LIMIT 3;
 -- 4) Qual o nome da subprefeitura com mais chamados abertos nesse dia?
 -- Resposta: Zona Norte.
 -- Query:
-SELECT bairros.subprefeitura as nome_subprefeitura FROM datario.administracao_servicos_publicos.chamado_1746 AS chamados
-JOIN datario.dados_mestres.bairro as bairros ON chamados.id_bairro = bairros.id_bairro
-WHERE DATE(data_inicio) = '2023-04-01'
+SELECT bairros.subprefeitura AS nome_subprefeitura FROM datario.administracao_servicos_publicos.chamado_1746 AS chamados
+JOIN datario.dados_mestres.bairro AS bairros ON chamados.id_bairro = bairros.id_bairro
+WHERE DATE(data_inicio) = '2023-04-01' AND data_particao = '2023-04-01'
 GROUP BY bairros.subprefeitura
 ORDER BY COUNT(*) DESC
 LIMIT 1;
@@ -41,14 +41,14 @@ LIMIT 1;
 -- Resposta: Existe 1 chamado que não possui id_bairro (id 18516246), logo, não possui bairro associado. Isso se deve pelo fato de ser um chamado do tipo "Ônibus", sendo assim, não possui necessariamente uma localização fixa para o chamado.
 -- Query:
 SELECT * FROM datario.administracao_servicos_publicos.chamado_1746 
-WHERE DATE(data_inicio) = '2023-04-01' AND id_bairro IS NULL;
+WHERE DATE(data_inicio) = '2023-04-01' AND data_particao = '2023-04-01' AND id_bairro IS NULL;
 
 
 -- 6) Quantos chamados com o subtipo "Perturbação do sossego" foram abertos desde 01/01/2022 até 31/12/2023 (incluindo extremidades)?
 -- Resposta: 42408 chamados.
 -- Query:
 SELECT COUNT(*) AS quantidade_chamados FROM datario.administracao_servicos_publicos.chamado_1746 
-WHERE subtipo = 'Perturbação do sossego' AND DATE(data_inicio) BETWEEN '2022-01-01' AND '2023-12-31';
+WHERE subtipo = 'Perturbação do sossego' AND data_particao BETWEEN '2022-01-01' AND '2023-12-01';
 
 
 -- 7) Selecione os chamados com esse subtipo que foram abertos durante os eventos contidos na tabela de eventos (Reveillon, Carnaval e Rock in Rio).
